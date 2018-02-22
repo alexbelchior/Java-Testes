ae740aab67b0ffd2f48a5438260cbe0e133e4c49
Alexandre Belchior <alexbelchior@gmail.com>
2018-02-22 16:09:21 -0300

    Update Decodificador.java

--- Decodificador.java
+++ Decodificador.java
@@ -12,38 +12,38 @@ public class Decodificador {
 		
 		String arquivo64 = javax.swing.JOptionPane.showInputDialog("digite aqui");
 		
-        /* Testar encode 64
-        File originalFile = new File("c:/xmlbvp/original.zip");
-        String encodedBase64 = null;
-        try {
-            FileInputStream fileInputStreamReader = new FileInputStream(originalFile);
-            byte[] bytes = new byte[(int)originalFile.length()];
-            fileInputStreamReader.read(bytes);
-            encodedBase64 = new String(Base64.getEncoder().encode(bytes));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        System.out.println(encodedBase64);
-        */
+		/* Testar encode 64
+		File originalFile = new File("c:/xmlbvp/original.zip");
+		String encodedBase64 = null;
+		try {
+		    FileInputStream fileInputStreamReader = new FileInputStream(originalFile);
+		    byte[] bytes = new byte[(int)originalFile.length()];
+		    fileInputStreamReader.read(bytes);
+		    encodedBase64 = new String(Base64.getEncoder().encode(bytes));
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+
+		System.out.println(encodedBase64);
+		*/
 		
-        File diretorio = new File("C:\\xmlbvp");
-        if(!diretorio.exists()){
-        	diretorio.mkdir();
-        }
-		
-        if(!arquivo64.isEmpty()){
-			byte[] arquivoDecodificado = Base64.getDecoder().decode(arquivo64);
-			System.out.println(arquivoDecodificado);
-			
-			FileOutputStream fos = new FileOutputStream("c:/xmlbvp/teste.zip");
-			fos.write(arquivoDecodificado);
-			fos.close();
-        }else {
-        	JOptionPane.showMessageDialog(null, "Por favor digite algo, o campo não pode estar em branco");
-        }
+		File diretorio = new File("C:\\xmlbvp");
+		if(!diretorio.exists()){
+			diretorio.mkdir();
+		}
+
+		if(!arquivo64.isEmpty()){
+				byte[] arquivoDecodificado = Base64.getDecoder().decode(arquivo64);
+				System.out.println(arquivoDecodificado);
+
+				FileOutputStream fos = new FileOutputStream("c:/xmlbvp/teste.zip");
+				fos.write(arquivoDecodificado);
+				fos.close();
+		}else {
+			JOptionPane.showMessageDialog(null, "Por favor digite algo, o campo não pode estar em branco");
+		}
 
 
 	}
